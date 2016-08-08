0a54282677c0f1ff55e12e339f1b3822f2779db3
Tom Fogal <tfogal@nvidia.com>
2016-08-08 14:59:16 -0700

    Add missing 'Range' method to stubs.
    Added it to the real class earlier but forgot it in the stub.

--- Source/PyFRStub/PyFRContourFilter.h
+++ Source/PyFRStub/PyFRContourFilter.h
@@ -1,6 +1,8 @@
 #ifndef PYFRCONTOURFILTER_H
 #define PYFRCONTOURFILTER_H
 
+#include <limits>
+#include <utility>
 class PyFRData;
 class PyFRContourData;
 
@@ -13,6 +15,11 @@ struct PyFRContourFilter
   void ClearContourValues() {}
 
   void SetContourField(int) {}
-}
-;
+  std::pair<float,float> Range() {
+    return std::make_pair(
+      std::numeric_limits<float>::signaling_NaN(),
+      std::numeric_limits<float>::signaling_NaN()
+    );
+  }
+};
 #endif
