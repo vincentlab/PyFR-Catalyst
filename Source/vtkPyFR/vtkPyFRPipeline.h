b5d9ec8650a98cc955ecc88773fa734b6fcae35e
Robert Maynard <robert.maynard@kitware.com>
2016-10-28 15:57:29 -0400

    Allow python to specify the field to contour and color by

--- Source/vtkPyFR/vtkPyFRPipeline.h
+++ Source/vtkPyFR/vtkPyFRPipeline.h
@@ -31,8 +31,12 @@ public:
   virtual void SetResolution(uint32_t w, uint32_t h);
   virtual void SetSpecularLighting(float coefficient, float power);
   virtual int CoProcess(vtkCPDataDescription* dataDescription);
+
   virtual void SetColorTable(const uint8_t* rgba, const float* loc, size_t n);
   virtual void SetColorRange(FPType, FPType);
+
+  virtual void SetFieldToContourBy(int);
+  virtual void SetFieldToColorBy(int);
 
   vtkSmartPointer<vtkSMSourceProxy> GetContour() { return this->Contour; }
   vtkSmartPointer<vtkSMSourceProxy> GetSlice()   { return this->Slice;   }
