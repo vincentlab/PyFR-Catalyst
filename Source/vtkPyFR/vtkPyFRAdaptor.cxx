7c02f87ccf41a6a0863c1ab3d6be150fd16e81a3
Robert Maynard <robert.maynard@kitware.com>
2016-11-02 10:05:07 -0400

    CatalystFinalize will not crash now.

--- Source/vtkPyFR/vtkPyFRAdaptor.cxx
+++ Source/vtkPyFR/vtkPyFRAdaptor.cxx
@@ -208,7 +208,9 @@ void CatalystFinalize(void* p)
 
   root(printf("[catalyst] finalizing %p at %s:%d\n", p, __FILE__, __LINE__));
 
+  if(pipeline->GetContour())
   { //release contour gpu memory
+  root(printf("[catalyst] finalizing %p at %s:%d\n", p, __FILE__, __LINE__));
   vtkObjectBase* clientSideContourBase = pipeline->GetContour()->GetClientSideObject();
   vtkPyFRContourFilter* realContour =
     vtkPyFRContourFilter::SafeDownCast(clientSideContourBase);
@@ -216,7 +218,9 @@ void CatalystFinalize(void* p)
   cData->ReleaseResources();
   }
 
+  if(pipeline->GetSlice())
   { //release slice gpu memory
+  root(printf("[catalyst] finalizing %p at %s:%d\n", p, __FILE__, __LINE__));
   vtkObjectBase* clientSideSliceBase = pipeline->GetSlice()->GetClientSideObject();
   vtkPyFRParallelSliceFilter* realSlice =
     vtkPyFRParallelSliceFilter::SafeDownCast(clientSideSliceBase);
